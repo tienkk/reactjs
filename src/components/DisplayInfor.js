7a8e2c863b1ce0908c0880a8df9977f976c81d65
tienkk <tiendong2kk2@gmail.com>
2024-06-05 16:09:06 +0700

    b10

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,34 +1,41 @@
 import React from "react";
 
 class DisplayInfor extends React.Component{
+
+    state = {
+        isShowListUser:true
+    }
+
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
+
     render(){
         const {listUsers} = this.props
-        console.log(listUsers)
         return(
             <div>
-                {listUsers.map((user) => {
-                    console.log(user)
-                    return (
-                        <div key={user.id}>
+                <div>
+                    <span onClick={() => {this.handleShowHide()}}>
+                        {this.state.isShowListUser === true ? "Hide list user" : "Show list user"}
+                    </span>
+                </div>
+                {this.state.isShowListUser &&
+                    <div>
+                        {listUsers.map((user) => {
+                            return (
+                            <div key={user.id} className={+user.age > 18 ? "green": "red" }>
                             <div>My name's {user.name}</div>
                             <div>My age's  {user.age}</div> 
                         </div>
-                    )
-                })}
-                {/* <div>My name's {name}</div>
-                <div>My age's {age}</div>
-                <hr />
-                <div>My name's {name}</div>
-                <div>My age's {age}</div>
-                <hr />
-                <div>My name's {name}</div>
-                <div>My age's {age}</div> */}
-
-            </div>
+                        )      
+                    })}
+                    </div>
+                }
+                </div>
         )
     }
-    
-
 }
 
 
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,9 +9,9 @@ class MyComponent extends React.Component{
 
     state ={
         listUsers:[
-            {id: 1, name :"tien",age: "26"},
-            {id: 2, name :"hung",age: "26"},
-            {id: 3, name :"tien",age: "26"},
+            {id: 1, name :"tien",age: "10"},
+            {id: 2, name :"hung",age: "40"},
+            {id: 3, name :"tien",age: "60"},
 
         ]
     }
